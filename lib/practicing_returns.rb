1dae6ef37941ebf7ef5262abecca39808bbed033
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-10 18:16:08 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -4,12 +4,11 @@ def hello(array)
   i = 0
   collection = []
   while i < array.length
-    collection <<
+    binding.pry
   yield(array[i])
     i += 1
   end
   collection
 end
 
-binding.pry
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
